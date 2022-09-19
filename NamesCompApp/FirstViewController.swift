1055c834f8d38d3940f8e1ac21e1b167c1e90bbd
techniquete666 <dodyaozxc@gmail.com>
2022-09-19 15:56:33 +0300

    Initial Commit

---
+++ NamesCompApp/FirstViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  NamesCompApp
+//
+//  Created by Vyacheslav on 25.08.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var partnerNameTF: UITextField!
+    @IBOutlet weak var yourNameTF: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destinationVC = segue.destination as? ResultViewController else { return }
+        destinationVC.firstName = yourNameTF.text
+        destinationVC.secondName = partnerNameTF.text
+    }
+
+    @IBAction func resultButtonTapped() {
+        guard let firstName = yourNameTF.text, let secondName = partnerNameTF.text else { return }
+        if firstName.isEmpty || secondName.isEmpty {
+            showAlert(
+                title: "Names are missing",
+                message: "Please enter both names ☺️"
+            )
+            return
+        }
+    
+        performSegue(withIdentifier: "goToResult", sender: nil)
+    }
+    
+    @IBAction func unwindSegueToFirstVC(segue: UIStoryboardSegue) {
+        
+        guard segue.identifier == "unwindSegue" else { return }
+        
+        yourNameTF.text = ""
+        partnerNameTF.text = ""
+    }
+    
+}
+
+extension FirstViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+extension FirstViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            resultButtonTapped()
+        }
+        return true
+    }
+}
